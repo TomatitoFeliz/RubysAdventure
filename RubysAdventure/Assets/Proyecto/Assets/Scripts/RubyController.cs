a5d6c9d60b999f2f2c312a4b4456a2feaf6dcc1b
TomatitoFeliz <>
2023-03-03 13:52:54 +0000

    BigUpdate

--- RubysAdventure/Assets/Proyecto/Assets/Scripts/RubyController.cs
+++ RubysAdventure/Assets/Proyecto/Assets/Scripts/RubyController.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    [SerializeField]
-    float velocidad;
+    public float velocidad;
+
+    public int maxHealth = 5;
+
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
+    Rigidbody2D rigidbody2d;
+
+    private void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
+    }
+
     private void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
+
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + velocidad * horizontal * Time.deltaTime;
         position.y = position.y + velocidad * vertical * Time.deltaTime;
-        transform.position = position;
+
+        rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
