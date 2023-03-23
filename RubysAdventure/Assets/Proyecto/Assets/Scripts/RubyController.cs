fbf252977a5f0f8a7ff0b7363af506dad8e20659
TomatitoFeliz <odassorodztomas@gmail.com>
2023-03-23 08:37:08 +0000

    FrogDialogue

--- RubysAdventure/Assets/Proyecto/Assets/Scripts/RubyController.cs
+++ RubysAdventure/Assets/Proyecto/Assets/Scripts/RubyController.cs
@@ -76,6 +76,19 @@ public class RubyController : MonoBehaviour
         {
             Launch();
         }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if (hit.collider != null)
+            {
+                NonPlayableCharacter character = hit.collider.GetComponent<NonPlayableCharacter>();
+                if (character != null)
+                {
+                    character.DisplayDialog();
+                }
+            }
+        }
     }
 
     public void ChangeHealth(int amount)
